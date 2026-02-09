3c7796bad5054a1b41b8443e1203fb1781ea3943
Sruthi <sruthivnair78@gmail.com>
2026-02-09 11:10:04 +0530

    edit

--- product-list/src/components/HeroSection.jsx
+++ product-list/src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 function HeroSection() {
   return (
-    <div className="relative h-[50vh] bg-[url('./hero.jpg')] bg-cover bg-right bg-no-repeat flex flex-col sm:items-start items-center sm:text-start text-center justify-center bg-slate-200 z-0 sm:px-12 px-3">
+    <div className="relative h-[50vh] bg-[url(/hero.jpg)] bg-cover bg-right bg-no-repeat flex flex-col sm:items-start items-center sm:text-start text-center justify-center bg-slate-200 z-0 sm:px-12 px-3">
       <div className="absolute inset-0 bg-gradient-to-r from-[rgba(224,242,254,0.6)] via-[rgba(224,242,254,0.4)] to-[rgba(224,242,254,0.2)] -z-10"></div>
 
       <div className="relative z-1">
